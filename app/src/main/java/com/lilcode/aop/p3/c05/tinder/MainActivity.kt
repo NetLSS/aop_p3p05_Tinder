917f926ade02988101797c5949c4a91fb1bcacad
NetLSS <futureprogrammer@naver.com>
2021-06-10 12:21:23 +0900

    Initial commit

---
+++ app/src/main/java/com/lilcode/aop/p3/c05/tinder/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.lilcode.aop.p3.c05.tinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
